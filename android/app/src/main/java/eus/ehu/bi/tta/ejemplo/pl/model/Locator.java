3e530b2e24c80603c105f5294ac526e0b21e5e09
Gorka Prieto <gorka.prieto@gmail.com>
2019-10-31 10:26:49 +0100

    Using MockBackend during the firsts lessons

--- android/app/src/main/java/eus/ehu/bi/tta/ejemplo/pl/model/Locator.java
+++ android/app/src/main/java/eus/ehu/bi/tta/ejemplo/pl/model/Locator.java
@@ -1,12 +1,12 @@
 package eus.ehu.bi.tta.ejemplo.pl.model;
 
 import eus.ehu.bi.tta.ejemplo.bl.backend.Backend;
-import eus.ehu.bi.tta.ejemplo.bl.backend.EhuBackend;
+import eus.ehu.bi.tta.ejemplo.bl.backend.MockBackend;
 
 public final class Locator {
     private static final UserModel userModel = new UserModel();
-    private static final Backend backend = new EhuBackend();
-    //private static final Backend backend = new MockBackend();
+    //private static final Backend backend = new EhuBackend();
+    private static final Backend backend = new MockBackend();
 
     public static UserModel getUserModel() {
         return userModel;
